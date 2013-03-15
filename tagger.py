fd256df343205bfe2f0e00bb2d66c0206a1eb015
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-03-15 14:44:51 +0100

    Port to a more GTK3 way of coding
    - We now have a menu when clicking on the name in the top bar
    - About dialog, basic, created
    - Do something with the stats button, more of an experiment to see how to code this

--- tagger.py
+++ tagger.py
@@ -25,59 +25,82 @@ main gnome-tagger file.
 import urllib2
 import json
 import requests
+import sys
 
-from gi.repository import Gtk, GdkPixbuf
+from gi.repository import Gtk, GdkPixbuf, Gio
 
 TAGGERAPI = 'http://209.132.184.171/'
 
 
-class GnomeTagger(object):
+class GnomeTaggerWindow(Gtk.ApplicationWindow):
 
-    def __init__(self):
+    def __init__(self, app):
+        window = Gtk.Window.__init__(self, title='GNOME Tagger', application=app)
+
         self.pkgname = None
 
+        
+        #self.set_default_size(400, 200)
+
+        # a grid to attach the toolbar (see below)
+        grid = Gtk.Grid()
+        self.add(grid)
+        # we have to show the grid (and therefore the toolbar) with show(),
+        # as show_all() would show also the buttons in the toolbar that we want to
+        # be hidden (such as the leave_fullscreen button)
+        grid.show()
+
+        # a builder to add the UI designed with Glade to the grid:
         self.builder = Gtk.Builder()
-        self.builder.add_from_file("tagger.ui")
-        window = self.builder.get_object("window1")
-        window.set_title("Tagger")
-        window.connect("delete-event", Gtk.main_quit)
-        window.show_all()
+        # get the file (if it is there)
+        try:
+            self.builder.add_from_file('tagger.ui')
+        except:
+            print 'file not found'
+            sys.exit()
+        # and attach it to the grid
+        grid.attach(self.builder.get_object('vbox1'), 0, 0, 1, 1)
+
+        # option 'about'
+        about_action = Gio.SimpleAction.new('about', None)
+        about_action.connect('activate', self.about_action)
+        app.add_action(about_action)
 
         settings = Gtk.Settings.get_default()
         settings.props.gtk_button_images = True
 
-        like_icon = GdkPixbuf.Pixbuf.new_from_file_at_size("like.png", 25, 25)
-        dislike_icon = GdkPixbuf.Pixbuf.new_from_file_at_size("dislike.png",
+        like_icon = GdkPixbuf.Pixbuf.new_from_file_at_size('like.png', 25, 25)
+        dislike_icon = GdkPixbuf.Pixbuf.new_from_file_at_size('dislike.png',
                                                               25, 25)
 
         image = Gtk.Image()
         image.set_from_pixbuf(like_icon)
         image.show()
-        likebutton = self.builder.get_object("button_like")
+        likebutton = self.builder.get_object('button_like')
         likebutton.add(image)
 
         image = Gtk.Image()
         image.set_from_pixbuf(dislike_icon)
         image.show()
-        dislikebutton = self.builder.get_object("button_dislike")
+        dislikebutton = self.builder.get_object('button_dislike')
         dislikebutton.add(image)
 
-        treeview = self.builder.get_object("treeview1")
+        treeview = self.builder.get_object('treeview1')
         treeselection = treeview.get_selection()
         treeselection.set_mode(Gtk.SelectionMode.MULTIPLE)
 
         cell_tag = Gtk.CellRendererText()
-        column_text = Gtk.TreeViewColumn("Tags")
+        column_text = Gtk.TreeViewColumn('Tags')
         column_text.pack_start(cell_tag, True)
         column_text.set_attributes(cell_tag, text=0)
         treeview.append_column(column_text)
 
         dic = {
-            "on_button_next_pkg_clicked": self.next_pkg_action,
-            "on_button_add_tag_clicked": self.add_tag_action,
-            "on_button_like_clicked": self.like_action,
-            "on_button_dislike_clicked": self.dislike_action,
-            "on_button_stats_clicked": self.stats_action,
+            'on_button_next_pkg_clicked': self.next_pkg_action,
+            'on_button_add_tag_clicked': self.add_tag_action,
+            'on_button_like_clicked': self.like_action,
+            'on_button_dislike_clicked': self.dislike_action,
+            'on_button_stats_clicked': self.stats_action,
         }
         self.builder.connect_signals(dic)
 
@@ -87,8 +110,8 @@ class GnomeTagger(object):
         """ Retrieve information about the next (random) package and update
         the GUI accordingly.
         """
-        print "next_pkg_action"
-        msg = self.builder.get_object("label_msg")
+        print 'next_pkg_action'
+        msg = self.builder.get_object('label_msg')
         msg.set_text('')
         self.get_package()
 
@@ -96,10 +119,10 @@ class GnomeTagger(object):
         """ Retrieve the tags from `entry_tag` and send them to the server,
         update the GUI afterward.
         """
-        print "add_tag_action"
-        msg = self.builder.get_object("label_msg")
+        print 'add_tag_action'
+        msg = self.builder.get_object('label_msg')
         msg.set_text('')
-        tagfield = self.builder.get_object("entry_tag")
+        tagfield = self.builder.get_object('entry_tag')
         entries = tagfield.get_text()
         entries = [entry.strip() for entry in entries.split(',')]
         tagfield.set_text('')
@@ -113,17 +136,19 @@ class GnomeTagger(object):
             else:
                 msg.set_text('\n'.join(jsonreq['messages']))
             self.get_package(self.pkgname)
+        else:
+            msg.set_text('No tag(s) to add')
 
     def like_action(self, *args, **kw):
         """ Retrieve the list of tags which are selected and send to the
         server that they have been 'licked'.
         If no tags are selected show an error dialog.
         """
-        print "like_click"
-        msg = self.builder.get_object("label_msg")
+        print 'like_click'
+        msg = self.builder.get_object('label_msg')
         msg.set_text('')
         data = {'pkgname': self.pkgname, 'vote': '1'}
-        treeview = self.builder.get_object("treeview1")
+        treeview = self.builder.get_object('treeview1')
         selection = treeview.get_selection()
         tree_model, rows = selection.get_selected_rows()
         for tree_iter in rows:
@@ -138,17 +163,19 @@ class GnomeTagger(object):
                     msg.set_text(jsonreq['error'])
                 else:
                     msg.set_text('\n'.join(jsonreq['messages']))
+        else:
+            msg.set_text('No tag(s) selected')
 
     def dislike_action(self, *args, **kw):
         """ Retrieve the list of tags which are selected and send to the
         server that they have been 'dislicked'.
         If no tags are selected show an error dialog.
         """
-        print "dislike_click"
-        msg = self.builder.get_object("label_msg")
+        print 'dislike_click'
+        msg = self.builder.get_object('label_msg')
         msg.set_text('')
         data = {'pkgname': self.pkgname, 'vote': '-1'}
-        treeview = self.builder.get_object("treeview1")
+        treeview = self.builder.get_object('treeview1')
         selection = treeview.get_selection()
         tree_model, rows = selection.get_selected_rows()
         for tree_iter in rows:
@@ -163,6 +190,8 @@ class GnomeTagger(object):
                     msg.set_text(jsonreq['error'])
                 else:
                     msg.set_text('\n'.join(jsonreq['messages']))
+        else:
+            msg.set_text('No tag(s) selected')
 
     def stats_action(self, *args, **kw):
         """ Retrieves statistics from the server and display them in a new
@@ -170,7 +199,25 @@ class GnomeTagger(object):
         These statistics include general statistics on the coverage of Tag
         per packages and the leaderboard of the game.
         """
-        print "stats_action"
+        print 'stats_action'
+        dialog = Gtk.Dialog()
+        dialog.set_title('Statistics')
+        # The window defined in the constructor (self) is the parent of the dialog.
+        # Furthermore, the dialog is on top of the parent window
+        dialog.set_transient_for(self)
+        # set modal true: no interaction with other windows of the application
+        dialog.set_modal(True)
+        # add a button to the dialog window
+        dialog.add_button(button_text='OK', response_id=Gtk.ResponseType.OK)
+        # connect the 'response' signal (the button has been clicked) to the function on_response()
+        dialog.connect('response', self.on_close)
+
+        # get the content area of the dialog, add a label to it
+        content_area = dialog.get_content_area()
+        label = Gtk.Label('This demonstrates a dialog with a label')
+        content_area.add(label)
+        # show the dialog
+        dialog.show_all()
 
     def get_package(self, name=None):
         """ Retrieve the information about a package if the name if set, a
@@ -193,28 +240,102 @@ class GnomeTagger(object):
         """
         self.pkgname = name
 
-        image = self.builder.get_object("image_pkg")
+        image = self.builder.get_object('image_pkg')
         response = urllib2.urlopen(icon_url)
         loader = GdkPixbuf.PixbufLoader()
         loader.write(response.read())
         image.set_from_pixbuf(loader.get_pixbuf())
         loader.close()
 
-        label = self.builder.get_object("label_pkg")
-        label.set_text("<b>%s</b>\n%s" % (name, summary or ''))
+        label = self.builder.get_object('label_pkg')
+        label.set_text('<b>%s</b>\n%s' % (name, summary or ''))
         label.set_use_markup(True)
 
-        treeview = self.builder.get_object("treeview1")
+        treeview = self.builder.get_object('treeview1')
         liststore = Gtk.ListStore(str)
         for tag in tags:
             liststore.append([tag])
         treeview.set_model(liststore)
 
+    def about_action(self, action, parameter):
+        """ Show the about window. """
+        aboutdialog = Gtk.AboutDialog()
+
+        # lists of authors and documenters (will be used later)
+        authors = ['Pierre-Yves Chibon', 'Ralph Bean']
+        #documenters = ['GNOME Documentation Team']
+
+        # we fill in the aboutdialog
+        aboutdialog.set_program_name('GNOME Tagger')
+        aboutdialog.set_copyright('Copyright \xc2\xa9 2013 Pierre-Yves Chibon')
+        aboutdialog.set_authors(authors)
+        #aboutdialog.set_documenters(documenters)
+        aboutdialog.set_comments('Tag the Fedora packages and win badges!')
+        aboutdialog.set_license('GPLv2 or any later version at your choice')
+        aboutdialog.set_website('http://github.com/fedora-infra/gnome-tagger')
+        aboutdialog.set_website_label('GNOME Tagger Website')
+
+        # we do not want to show the title, which by default would be 'About AboutDialog Example'
+        # we have to reset the title of the messagedialog window after setting the program name
+        aboutdialog.set_title('')
+
+        # to close the aboutdialog when 'close' is clicked we connect the
+        # 'response' signal to on_close
+        aboutdialog.connect('response', self.on_close)
+        # show the aboutdialog
+        aboutdialog.show()
+
+    def on_close(self, action, parameter):
+        """ Called to close the about dialog. """
+        action.destroy()
+
+
+class GnomeTagger(Gtk.Application):
+
+    def __init__(self):
+        Gtk.Application.__init__(self)
+
+    def do_activate(self):
+        win = GnomeTaggerWindow(self)
+        win.show_all()
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+        # create a menu
+        menu = Gio.Menu()
+        # append to the menu three options
+        menu.append('Preferences', 'app.preference')
+        menu.append('About', 'app.about')
+        menu.append('Quit', 'app.quit')
+        # set the menu as menu of the application
+        self.set_app_menu(menu)
+
+        # option 'preference'
+        new_action = Gio.SimpleAction.new('preference', None)
+        new_action.connect('activate', self.preference_action)
+        self.add_action(new_action)
+
+        # option 'quit'
+        quit_action = Gio.SimpleAction.new('quit', None)
+        quit_action.connect('activate', self.quit_action)
+        self.add_action(quit_action)
+
+    # callback function for 'new'
+    def preference_action(self, action, parameter):
+        print 'preference clicked'
+
+    # callback function for 'quit'
+    def quit_action(self, action, parameter):
+        print 'You have quit.'
+        self.quit()
+
 
 def main():
     """ Main function. """
-    GnomeTagger()
-    Gtk.main()
+    app = GnomeTagger()
+    exit_status = app.run(sys.argv)
+    sys.exit(exit_status)
 
 
 if __name__ == '__main__':
